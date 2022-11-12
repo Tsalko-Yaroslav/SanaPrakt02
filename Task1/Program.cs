0564398caa1f3bd0594b0122f7d347495d82c61b
Yaroslav Tsalko <ipz212_tsyav@student.ztu.edu.ua>
2022-11-12 18:21:35 +0200

    1-3 txt change

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Херсон наш!");
+Console.WriteLine("Слава Україні!");
+Console.WriteLine("Героям слава!");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
